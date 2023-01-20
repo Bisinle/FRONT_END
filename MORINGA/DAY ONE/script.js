5c66d68c94914974579d039013c99910e04dbf02
bisinle <abdiwadud.mohamedd@gmail.comm>
2023-01-20 09:38:40 +0300

    budlep up  changes

--- MORINGA/DAY ONE/script.js
+++ MORINGA/DAY ONE/script.js
@@ -8,5 +8,37 @@ const options = {
 
 fetch('https://moviesdatabase.p.rapidapi.com/titles/x/upcoming', options)
 	.then(response => response.json())
-	.then(response => console.log(response))
+	.then((response) =>{
+		Object.values(response).forEach(arr =>{
+			//console.log(arr);
+			for(key in arr){
+				console.log(arr[key])
+			}
+		})
+	})
+
+
 	.catch(err => console.error(err));
+
+
+/*
+
+	const studname = (fname, lname)=>{
+		console.log(fname);
+		console.log(lname)
+		
+	}
+
+
+	function studname(fname,lname){
+		console.log(fname);
+		console.log(lname)
+
+
+	}
+
+	const studname=function(fnamem,lname) {
+		console.log(fname);
+		console.log(lname);
+		
+	}*/
